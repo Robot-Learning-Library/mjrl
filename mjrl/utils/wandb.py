cdc9fb18e676deb31f8f7e1775d8b472faace3fa
zihan <zhding@mail.ustc.edu.cn>
2022-09-07 12:47:18 -0400

    fix bug in wandb

--- mjrl/utils/wandb.py
+++ mjrl/utils/wandb.py
@@ -1,12 +1,25 @@
 import wandb
 
 def init_wandb(args):
-    wandb.init(
-        project=args['wandb_project'],
-        entity=args['wandb_entity'],
-        sync_tensorboard=True,
-        config=args,  # dict type
-        name=args['wandb_name'],
-        monitor_gym=True,
-        save_code=True,
-    )
+    try:
+        wandb.init(
+            project=args['wandb_project'],
+            entity=args['wandb_entity'],
+            sync_tensorboard=True,
+            config=args,  # dict type
+            name=args['wandb_name'],
+            monitor_gym=True,
+            save_code=True,
+        )
+    except:
+        wandb.init(
+            project=args.wandb_project,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),  # namespace to dict type
+            name=args.wandb_name,
+            monitor_gym=True,
+            save_code=True,
+        )        
+
+
